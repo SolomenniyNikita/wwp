6ba9c34112e8e67b5cf5409829cfea3f5ae15a4f
SolomenniyNikita <75935797+SolomenniyNikita@users.noreply.github.com>
2021-01-17 18:24:46 +0300

    Add files via upload

---
+++ №10 7.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+int main(void)
+{
+    float a, b, c;
+    printf("a\n");
+    scanf_s("%f", &a);
+    printf("b\n");
+    scanf_s("%f", &b);
+    printf("c\n");
+    scanf_s("%f", &c);
+    printf("%d\n", (a < b + c) & (b < a + c) & (c < a + b));
+    return 0;
+}
